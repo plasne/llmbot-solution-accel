d9e24bba2bcb431b48708822a1dda28e689a7c4d
Peter Lasne <plasne@iname.com>
2024-05-02 09:07:12 -0400

    working on determine intent

--- sk/Ext.cs
+++ sk/Ext.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Azure.AI.OpenAI;
 using DistributedChat;
 using Microsoft.SemanticKernel.ChatCompletion;
 
-public static class Ext
+public static partial class Ext
 {
     public static ChatHistory ToChatHistory(this List<Turn> turns)
     {
@@ -23,4 +26,35 @@ public static class Ext
 
         return history;
     }
+
+    public static List<ChatRequestMessage> ToChatRequestMessages(this string template)
+    {
+        var messages = new List<ChatRequestMessage>();
+        var regex = MessageRegex();
+        var matches = regex.Matches(template);
+
+        foreach (Match match in matches)
+        {
+            var role = match.Groups[1].Value.ToLower();
+            var text = match.Groups[2].Value;
+
+            switch (role)
+            {
+                case "system":
+                    messages.Add(new ChatRequestSystemMessage(text));
+                    break;
+                case "user":
+                    messages.Add(new ChatRequestUserMessage(text));
+                    break;
+                case "assistant":
+                    messages.Add(new ChatRequestAssistantMessage(text));
+                    break;
+            }
+        }
+
+        return messages;
+    }
+
+    [GeneratedRegex("<message role=\"(.*?)\">(.*?)</message>", RegexOptions.Singleline)]
+    private static partial Regex MessageRegex();
 }
--- sk/steps/01-DetermineIntent.cs
+++ sk/steps/01-DetermineIntent.cs
@@ -9,6 +9,13 @@ using System.Threading;
 using Azure.AI.OpenAI;
 using SharpToken;
 using System;
+using DistributedChat;
+using Azure;
+using Microsoft.Extensions.Options;
+using Azure.Search.Documents.Models;
+using System.Text.Json;
+using HandlebarsDotNet;
+using Xunit.Sdk;
 
 public class DetermineIntent(
     IConfig config,
@@ -45,40 +52,98 @@ public class DetermineIntent(
             return File.ReadAllTextAsync("prompts/intent.txt");
         });
 
-        // build the function
-        var func = this.kernel.CreateFunctionFromPrompt(
-            new()
-            {
-                Template = template,
-                TemplateFormat = "handlebars"
-            },
-            new HandlebarsPromptTemplateFactory()
-        );
-
-        // add prompt token count reporting using sharpToken
-        var promptTokenCount = 0;
-        kernel.PromptFilters.Add(new PromptTokenCountFilter(this.config.LLM_MODEL_NAME, count =>
-        {
-            promptTokenCount = count;
-            DiagnosticService.RecordPromptTokenCount(count, this.config.LLM_MODEL_NAME, this.GetType().Name);
-        }));
-
         // build the history
         ChatHistory history = input.History?.ToChatHistory() ?? [];
 
-        // execute
-        var startTime = DateTime.UtcNow;
-        var response = await this.kernel.InvokeAsync(
-            func,
-            new()
+        // render the prompt
+        var f = new HandlebarsPromptTemplateFactory();
+        var q = f.Create(new PromptTemplateConfig
+        {
+            Template = template,
+            TemplateFormat = "handlebars"
+        });
+        var prompt = await q.RenderAsync(this.kernel, new()
             {
                 { "history", history },
                 { "query", input.UserQuery },
+            }, cancellationToken: cancellationToken);
+
+        var determineIntentDefinition = new ChatCompletionsFunctionToolDefinition()
+        {
+            Name = "determine_intent",
+            Description = "Determines the intent of the user.",
+            Parameters = BinaryData.FromObjectAsJson(
+            new
+            {
+                Type = "object",
+                Properties = new
+                {
+                    Intent = new
+                    {
+                        Type = "string",
+                        Enum = new[] { "UNKNOWN", "GREETING", "GOODBYE", "IN_DOMAIN", "OUT_OF_DOMAIN", "TOPIC_CHANGE" },
+                    },
+                    Query = new
+                    {
+                        Type = "string"
+                    },
+                    SearchQueries = new
+                    {
+                        Type = "array",
+                        Items = new
+                        {
+                            Type = "string"
+                        }
+                    },
+                    GameName = new
+                    {
+                        Type = "string"
+                    },
+                    Edition = new
+                    {
+                        Type = "string"
+                    }
+                },
+                Required = new[] { "intent", "query", "search_queries" },
             },
-            cancellationToken);
+            new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower }),
+        };
+
+        var client = new OpenAIClient(new Uri(this.config.LLM_ENDPOINT_URI), new AzureKeyCredential(this.config.LLM_API_KEY));
+        var options = new ChatCompletionsOptions
+        {
+            DeploymentName = this.config.LLM_DEPLOYMENT_NAME,
+            ToolChoice = new ChatCompletionsToolChoice(determineIntentDefinition),
+        };
+        foreach (var message in prompt.ToChatRequestMessages())
+        {
+            options.Messages.Add(message);
+        }
+        options.Tools.Add(determineIntentDefinition);
+        var response = await client.GetChatCompletionsAsync(options, cancellationToken);
+
+        var choice = response.Value.Choices[0];
+        if (choice.Message.ToolCalls is null || choice.Message.ToolCalls.Count != 1)
+        {
+            throw new Exception("Unexpected tool calls.");
+        }
+
+        var toolCall = choice.Message.ToolCalls[0] as ChatCompletionsFunctionToolCall;
+        if (toolCall is null)
+        {
+            throw new Exception("tool call is null.");
+        }
+        var intent = JsonConvert.DeserializeObject<DeterminedIntent>(toolCall.Arguments);
+
+        this.logger.LogWarning(JsonConvert.SerializeObject(choice));
+
+
+        // execute
+        var startTime = DateTime.UtcNow;
         var elapsedSeconds = (DateTime.UtcNow - startTime).TotalSeconds;
 
         // record completion token count using sharpToken
+        /*
         var completionTokenCount = 0;
         if (response.Metadata is not null && response.Metadata.TryGetValue("Usage", out var usageOut) && usageOut is CompletionsUsage usage)
         {
@@ -97,17 +162,20 @@ public class DetermineIntent(
             var tokensPerSecond = completionTokenCount / elapsedSeconds;
             DiagnosticService.RecordTokensPerSecond(tokensPerSecond, this.config.LLM_MODEL_NAME, this.GetType().Name);
         }
+        */
 
         // deserialize the response
         // NOTE: this could maybe be a retry (transient fault)
+        /*
         var intent = JsonConvert.DeserializeObject<DeterminedIntent>(response.ToString())
             ?? throw new HttpException(500, "Intent could not be deserialized.");
+        */
 
         // if in debug mode, log the intent
-        this.logger.LogDebug(response.ToString());
+        // this.logger.LogDebug(response.ToString());
 
         // send token counts
-        await this.context.Stream(promptTokens: promptTokenCount, completionTokens: completionTokenCount);
+        // await this.context.Stream(promptTokens: promptTokenCount, completionTokens: completionTokenCount);
 
         // record to context
         return intent;
