d9f9626bac4aea6f7f83bbb59528ea93789d1d69
TechnicallyWilliams <DexterW@microsoft.com>
2024-05-17 09:13:05 -0500

    add tpot to bot

--- bot/services/ChatBot.cs
+++ bot/services/ChatBot.cs
@@ -301,8 +301,9 @@ public class ChatBot(
                 // handle the response
                 activityId = await this.HandleResponse(turnContext, activityId, chatResponse, summaries, completeRequest, cancellationToken);
             }
-            completeRequest.TimeToLastResponse = (int)(DateTime.UtcNow - started).TotalMilliseconds;
+            completeRequest.TimeToLastResponse = (int)(DateTime.UtcNow - this.started).TotalMilliseconds;
             DiagnosticService.RecordTimeToLastResponse(completeRequest.TimeToLastResponse);
+            DiagnosticService.RecordTimePerOutputToken((double)(completeRequest.TimeToLastResponse - completeRequest.TimeToFirstResponse) / completeRequest.CompletionTokenCount);
 
             // write the generated message
             completeRequest.Message = completeRequest.State != States.EMPTY
